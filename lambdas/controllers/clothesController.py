04494d2dfe5d697bdc55d59c42084c55b433791a
kindadailybren <bomedina1@up.edu.ph>
2026-04-05 23:46:14 +0800

    feat: initial auth and clothes routes

--- lambdas/controllers/clothesController.py
+++ lambdas/controllers/clothesController.py
@@ -2,3 +2,8 @@ from fastapi import APIRouter, Depends, Request
 from fastapi import HTTPException
 
 clothesRouter = APIRouter()
+
+
+@clothesRouter.get("/clothes")
+async def getClothes():
+    return
